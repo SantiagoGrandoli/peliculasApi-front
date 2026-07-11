a2b9268ea5012694a1ada3740dd11c8011944845
NachoUhrin <nachouhrin@gmail.com>
2026-07-11 02:05:35 -0300

    Se realizaron cambios para la conexion al BE

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import AppRouter from './AppRouter'
 
-export default function App() {
-  return <AppRouter />
+import { useEffect } from "react";
+import api from "./services/api";
+
+function App() {
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const response = await api.get("/movies");
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getMovies();
+  }, []);
+
+  return (
+    <>
+      <AppRouter />
+      <h1>Probando API</h1>
+    </>
+  );
 }
+
+export default App;
